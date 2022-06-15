728efe02cc3ad129789a6ea7b13e6d378c06347f
NickosDalkidis <90420049+NickosDalkidis@users.noreply.github.com>
2022-06-15 16:31:38 +0300

    Add files via upload

---
+++ Player leve1l.py
@@ -0,0 +1,59 @@
+from _ast import If
+
+print("Hello and welcome to this app ")
+print("In this app you find if you know anything about videogames in general")
+print("Are You Ready For The test?")
+ready = input("Please Enter Your Answer Yes/No")
+bop="Yes"
+bopp="No"
+if ready:=bopp:
+    print("Thanks for checking my code")
+if ready:=bop:
+    print("You will be asked 10 questions")
+    print("For every right answer you get three points whilst for every wrong answer you will lose 2 points")
+    print("1.Which Real Madrid star is on the cover of FIFA 20?")
+    print("a.Gareth Bale"
+          " b.Eden Hazard"
+          " c.James Rodriguez"
+          " d.Sergio Ramos")
+    print("2.What colour is the blue ghost in Pac-Man?")
+    print("a.Blinky"
+          " b.Inky"
+          " c.Pinky"
+          " d.Clyde")
+    print("3.What's the name of the theme song in Crossy Road?")
+    print("a.Crossroads"
+          " b.Old Town Road"
+          " c.Road To nowhere"
+          " d.There's no music in the game")
+    print("4.What language do The Sims characters speak?")
+    print("a.Finnish"
+          " b.Simlish"
+          " c.Flemish"
+          " d.Dutch")
+    print("5.There's a basketball version of Rocket League called Hoopmania. True or false?")
+    print("a.That has got to be absolutely right"
+          " b.False -it's called Hoops")
+    print("6.When was the first Nintendo console released?")
+    print("a.1963"
+          " b.1980"
+          " c.1983"
+          " d.2001")
+    print("7.In the Game Boy Advance version of Super Mario World, who can jump higher? Mario or Luigi? ")
+    print("a.Mario"
+          " b.Luigi")
+    print("8.How many Legend of Zelda games are there?")
+    print("a.5"
+          " b.8"
+          " c.10"
+          " d.15")
+    print("9.Which country invented PlayStation?")
+    print("a.China"
+          " b.South Korea"
+          " c.Japan"
+          " d.Vietman")
+    print("10.How many Super Mario Maker games have been released on PlayStation?")
+    print("a.0"
+          " b.1"
+          " c.2"
+          " d.3")
