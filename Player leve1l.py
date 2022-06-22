deb101725aee2bb6a1b6eec07ee267932f4054ec
NickosDalkidis <90420049+NickosDalkidis@users.noreply.github.com>
2022-06-22 15:57:58 +0300

    Update Player leve1l.py

--- Player leve1l.py
+++ Player leve1l.py
@@ -16,44 +16,63 @@ if ready:=bop:
           " b.Eden Hazard"
           " c.James Rodriguez"
           " d.Sergio Ramos")
+    ans1=input("Please enter the correct answer")
+
     print("2.What colour is the blue ghost in Pac-Man?")
     print("a.Blinky"
           " b.Inky"
           " c.Pinky"
           " d.Clyde")
+    ans2=input("Please enter the correct answer") 
+
     print("3.What's the name of the theme song in Crossy Road?")
     print("a.Crossroads"
           " b.Old Town Road"
           " c.Road To nowhere"
           " d.There's no music in the game")
+    ans3=input("Please enter the correct answer")
+
     print("4.What language do The Sims characters speak?")
     print("a.Finnish"
           " b.Simlish"
           " c.Flemish"
           " d.Dutch")
+    ans4=input("Please enter the correct answer")
+
     print("5.There's a basketball version of Rocket League called Hoopmania. True or false?")
     print("a.That has got to be absolutely right"
           " b.False -it's called Hoops")
+    ans5=input("Please enter the correct answer")
+
     print("6.When was the first Nintendo console released?")
     print("a.1963"
           " b.1980"
           " c.1983"
           " d.2001")
+    ans6=input("Please enter the correct answer")
+
     print("7.In the Game Boy Advance version of Super Mario World, who can jump higher? Mario or Luigi? ")
     print("a.Mario"
           " b.Luigi")
+    ans7=input("Please enter the correct answer")
+
     print("8.How many Legend of Zelda games are there?")
     print("a.5"
           " b.8"
           " c.10"
           " d.15")
+    ans8=input("Please enter the correct answer") 
+
     print("9.Which country invented PlayStation?")
     print("a.China"
           " b.South Korea"
           " c.Japan"
           " d.Vietman")
+    ans9=input("Please enter the correct answer")
+
     print("10.How many Super Mario Maker games have been released on PlayStation?")
     print("a.0"
           " b.1"
           " c.2"
           " d.3")
+    ans10=input("Please enter the correct answer")      
